3d3d43700d14d6901bd658875f17c6282851a3f4
ablancoabalde <ec2-user@ip-172-31-22-149.us-east-2.compute.internal>
2018-11-27 09:49:13 +0000

    Commit del 404

--- 404.php
+++ 404.php
@@ -15,14 +15,16 @@ get_header(); ?>
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<!-- Modificación de la pagina 404 -->
 			<section class="error-404 not-found">
 				<header class="page-header">
+					<!-- En un titulo le meto un mensaje de error -->
 					<h1 class="page-title"><?php _e( 'Oops! cagada algo falló, lo siento.', 'twentyseventeen' ); ?></h1>
 					<!DOCTYPE html>
 				
 				</header><!-- .page-header -->
 				<div class="page-content">
+					<!-- En un parrafo le meto un mensaje de error y luego una imagen -->
 					<p><?php _e( 'Por algún motivo no encontre nada, vuelva a intentarlo', 'twentyseventeen' ); ?></p>
 					<img src="https://3hti.com/wp-content/uploads/2017/11/thingworx-studio-thingmark.png"></img>
 					<?php get_search_form(); ?>
